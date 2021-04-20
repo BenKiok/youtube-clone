3bdb0118b43941dff5fa376cb4ec6b789943d1f7
BenKiok <benedictmkioko@gmail.com>
2021-04-20 09:23:21 -0400

    Style nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,31 @@
+import '../styling/Nav.css';
+
 function Nav() {
   return (
     <div className='nav'>
       <nav>
+      <div className='nav-main'>
+        <i className='fas fa-bars'/>
+        <span className='nav-youtube'>
+          <i className='fab fa-youtube'/>
+          YouTube
+        </span>
+      </div>
+      <div className='search-bar'>
+        <input type='text' placeholder='Search'/>
+        <button>
+          <i className='fas fa-search'/>
+        </button>
+        <i className='fas fa-microphone'/>
+      </div>
+      <div className='nav-other'>
+        <i className='fas fa-video'/>
+        <i className='fas fa-th'/>
+        <i className='fas fa-bell'/>
+        <span className='user'>
+          B
+        </span>
+      </div>
       </nav>
     </div>
   );
